cb6f7f3a1c57085e29997d64505afcc1e3f0c46b
SMGeyser <samgeyser@gmail.com>
2026-05-11 14:51:56 -0600

    5/11 Responsive ponder

---
+++ ponder/responsive.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("nav")
+let button = document.querySelector(".menu-btn")
+
+button.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    button.classList.toggle('change');
+}
+
