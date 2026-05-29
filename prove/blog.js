031401470fe210c9dac6021a0660370153daf75d
SMGeyser <samgeyser@gmail.com>
2026-05-29 12:04:42 -0600

    prove 5/29

--- prove/blog.js
+++ prove/blog.js
@@ -1,15 +1,66 @@
+
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+	}
+];
+                
+
 let bookList = document.querySelector('#book-list');
 
-let html = `<article class="book">
-        <hr>
-        <p class="date">July 5, 2022</p>
-        <p class="age">10-14</p>
-        <p class="genre">Fantasy</p>
-        <p><span aria-label="four out of five stars" role="none">⭐⭐⭐⭐</span></p>
-        <h2>Septimus Heap Book One: Magyk</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg" alt="Book cover of the Septimus Heap Book One: Magic. With a gold ring on the cover.">
-        <p class='desc'>If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.</p>
-        </article>`
+articles.forEach(function(book){
 
-bookList.innerHTML = html;
+        const blog = document.createElement('article');
+        blog.className = 'book';
+
+        let html = `
+        <hr>
+        <p class="date">${book.date}</p>
+        <p class="age">${book.ages}</p>
+        <p class="genre">${book.genre}</p>
+        <p><span aria-label="${book.stars.length} out of five stars" role="none">${book.stars}</span></p>
+        <h2>${book.title}</h2>
+        <img src="${book.imgSrc}" alt="${book.imgAlt}">
+        <p class='desc'>${book.description}</p>
+        `;
+
+        blog.innerHTML = html;
+        bookList.appendChild(blog);
+})
+
 
